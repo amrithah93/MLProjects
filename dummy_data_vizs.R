4c581592f296103ca1a8ef44bd107db1ba46c730
Amritha Harikumar <65935651+amrithah93@users.noreply.github.com>
2025-06-13 20:14:27 -0400

    Add files via upload

---
+++ dummy_data_vizs.R
@@ -0,0 +1,10 @@
+library(hrbrthemes)
+library(GGally)
+data(iris)
+
+# Quick display of two cabapilities of GGally, to assess the distribution and correlation of variables 
+library(GGally)
+
+# From the help page:
+data(iris)
+ggpairs(iris, columns = 2:4, ggplot2::aes(colour=Species)) 
